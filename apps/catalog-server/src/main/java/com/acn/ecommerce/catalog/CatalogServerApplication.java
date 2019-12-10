e4e01e3ee9a8ff664dc2a2cb7ec552357448abce
yeremia-d <yeremia.djaja@gmail.com>
2019-12-10 10:16:59 -0500

    first code commit. catalog server & gradle muli-module project setup

---
+++ apps/catalog-server/src/main/java/com/acn/ecommerce/catalog/CatalogServerApplication.java
@@ -0,0 +1,12 @@
+package com.acn.ecommerce.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServerApplication.class, args);
+    }
+}
